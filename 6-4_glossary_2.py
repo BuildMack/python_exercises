fe1f76ccdd8e4afa2f018b2c13b8265eb043dcb9
Mack Sell <m.m.sell@protonmail.com>
2021-12-15 23:57:49 -0500

    Completed 6-4

---
+++ 6-4_glossary_2.py
@@ -0,0 +1,12 @@
+# Exercise 6-4 from Python Crash Course Book
+# Completed by Mack Sell
+
+glossary = {
+    'tuple': 'An immutable list.',
+    'conditional expression': 'An expression that evaluates to True or False.',
+    'immutable': 'Unable to be altered.',
+    'PEP8': 'Python Enhancement Proposal 8'
+    }
+
+for word in glossary:
+    print(f'{word.title()}:\n{glossary[word]}\n')
