83aafafc50c904db3c373be48fc38382fae969f5
Mack Sell <m.m.sell@protonmail.com>
2021-12-29 00:02:09 -0500

    6-10 complete

--- chapter_6/6-10_favorite_numbers.py
+++ chapter_6/6-10_favorite_numbers.py
@@ -1,2 +1,21 @@
 # Exercise 6-10 from Python Crash Course Book
 # Completed by Mack Sell
+
+favourite_numbers = {
+    'Sharon': [16, 3],
+    'Dan': [2, 6, 30],
+    'Curtis': [31, 2, 5], 
+    'Mitchell': [24,4]
+    }
+
+
+
+for favourite_number in favourite_numbers:
+    print(f"\n{favourite_number}'s favourite numbers are:") 
+    numbers = ''
+    
+    for number in favourite_numbers[favourite_number]:
+        numbers += str(number) + " and "
+    
+    print(numbers[:-5])
+    
