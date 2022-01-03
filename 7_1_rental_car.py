3a0d80f8f38f2f76e67cf64122092dc1bb0631ba
Mack Sell <m.m.sell@protonmail.com>
2022-01-02 23:46:03 -0500

    7_1 completed

---
+++ 7_1_rental_car.py
@@ -0,0 +1,10 @@
+# Exercise 7-1 from Python Crash Course Book
+# Completed by Mack Sell
+
+car_type = input('What type of rental car would you like? ')
+
+print(f"Let me see if I can find you a {car_type.title()}.")
+
+car_color = input('What color would you like? ')
+
+print(f"Okay, I will try to find you a {car_color.lower()} {car_type.title()}.")
