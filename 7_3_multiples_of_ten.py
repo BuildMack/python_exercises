909b8ca6fc37039539d7ed44b652e3c63b20a348
Mack Sell <m.m.sell@protonmail.com>
2022-01-03 23:57:56 -0500

    7_5 complete

--- 7_3_multiples_of_ten.py
+++
@@ -1,9 +0,0 @@
-# Exercise 7-3 from Python Crash Course Book
-# Completed by Mack Sell
-
-number = input('Enter a number please: ')
-
-if number % 10 == 0:
-    print('Your number is a multiple of ten.')
-else:
-    print('Your number is not a multiple of ten.')
