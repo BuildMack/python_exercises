a5c69f7e2562e9e911c40e2986376a81f41d730f
Mack Sell <m.m.sell@protonmail.com>
2022-01-05 23:29:43 -0500

    7-8 complete

---
+++ chapter_7/7_8_deli.py
@@ -0,0 +1,15 @@
+# Exercise 7-7 from Python Crash Course Book
+# Completed by Mack Sell
+
+sandwich_orders = ['ruben', 'tomato', 'ham', 'turkey', 'cucumber']
+
+sandwich_finished = []
+
+while sandwich_orders != []:
+    print(f'\nYour {sandwich_orders[0]} sandwich has been completed.')
+    sandwich_finished.append(sandwich_orders[0])
+    sandwich_orders.remove(sandwich_orders[0])
+
+print(f'\nCurrent orders: {sandwich_orders}')
+
+print(f'\nCompleted sandwich: {sandwich_finished}')
