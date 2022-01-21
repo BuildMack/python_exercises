7ddc15ffff9b5bff0b204f5aa114cc41ce59f7fa
Mack Sell <m.m.sell@protonmail.com>
2022-01-20 23:57:16 -0500

    Added optional year input

--- chapter_8/8_7_albums.py
+++ chapter_8/8_7_albums.py
@@ -1,11 +1,12 @@
 # Exercise 8-6 from Python Crash Course Book
 # Completed by Mack Sell
 
-def make_albums(artist, album, songs = None):
+def make_albums(artist, album, songs = None, year = None):
     music = {
         'artist': artist,
         'album': album,
-        'number of songs': songs
+        'number of songs': songs,
+        'year': year
         }
     return music
 
@@ -13,4 +14,4 @@ print(make_albums('Prince','Purple Rain', 8))
 
 print(make_albums('ACDC','Back in Black'))
 
-print(make_albums('Led Zepplin','Mothership'))
+print(make_albums('Led Zepplin','Mothership', year=1966))
