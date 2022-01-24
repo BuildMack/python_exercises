60a7d58ae4cc4e1b386f0a2833d02324ed71da4d
Mack Sell <m.m.sell@protonmail.com>
2022-01-23 23:55:04 -0500

    8_10 complete

---
+++ chapter_8/8_10_sending_messages.py
@@ -0,0 +1,15 @@
+conversation = [
+    'hi', 'hey','how are you?', 'good, hbu?', 
+    'great, want to meet at 5?', 'yes, sounds good!'           
+    ]
+
+def send_messages(texts):
+    print('This is your conversation:')
+    sent_texts=[]
+    while texts:
+        text = texts.pop(0)
+        print(text)
+        sent_texts.append(text)
+    return sent_texts
+
+print(send_messages(conversation))
