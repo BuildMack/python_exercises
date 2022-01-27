5e473359245277ae69f078c5d30888f9426cf691
Mack Sell <m.m.sell@protonmail.com>
2022-01-26 23:54:45 -0500

    Added comments to the top explaining script

--- chapter_8/8_10_sending_messages.py
+++ chapter_8/8_10_sending_messages.py
@@ -1,3 +1,6 @@
+# Exercise 8-10 from Python Crash Course Book
+# Completed by Mack Sell
+
 conversation = [
     'hi', 'hey','how are you?', 'good, hbu?', 
     'great, want to meet at 5?', 'yes, sounds good!'           
--- chapter_8/8_11_archive_messages.py
+++ chapter_8/8_11_archive_messages.py
@@ -1,3 +1,7 @@
+# Exercise 8-11
+#  from Python Crash Course Book
+# Completed by Mack Sell
+
 conversation = [
     'hi', 'hey','how are you?', 'good, hbu?', 
     'great, want to meet at 5?', 'yes, sounds good!'           
