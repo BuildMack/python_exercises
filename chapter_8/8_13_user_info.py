f0410bef546261c87e254f0b6157c1cf5045c757
Mack Sell <m.m.sell@protonmail.com>
2022-02-10 23:44:36 -0500

    Added a default input to the funtion in 8_13

--- chapter_8/8_13_user_info.py
+++ chapter_8/8_13_user_info.py
@@ -1,14 +1,14 @@
 # Exercise 8-13 from Python Crash Course Book
 # Completed by Mack Sell
 
-def build_profile(first,last, **user_info):
+def build_profile(first,last,location = 'princeton',  **user_info):
     """Build a dictionary, contraining everything we know about a user."""
     user_info['first_name'] = first
     user_info['last_name'] = last
+    user_info['location'] = location
     return user_info
 
 user_profile = build_profile('albert','einstein',
-                             location = 'princeton',
                              field = 'physics')
 
 print(user_profile)
